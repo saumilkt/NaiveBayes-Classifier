862b98540681ea3c83569548923db70fa5a3e9c4
Saumil Thakore <saumilkt@gmail.com>
2020-10-13 18:49:04 -0700

    Added function to calculate pixel probabilities

--- src/core/probability_discerner.cc
+++ src/core/probability_discerner.cc
@@ -156,6 +156,47 @@ void probability_discerner::ImportData(const std::string &data_path,
   CalculateProbabilities();
 }
 
+void probability_discerner::CalculateProbabilities() {
+  for (int digit = kFirstDigit; digit <= kLastDigit; digit++) {
+    // Addition of elements of the tuple in any coordinate
+    // with same digit will always be the same and reflect the number of
+    // examples that digit has.
+    int num_digit_repeat =
+        std::get<kBlackIndex>(data_set_[digit][std::make_pair(0, 0)]) +
+        std::get<kWhiteIndex>(data_set_[digit][std::make_pair(0, 0)]) +
+        std::get<kGrayIndex>(data_set_[digit][std::make_pair(0, 0)]) ;
+
+    // Probability of each digit is calculated and added to a class var.
+    double prob_digit = (kClassifyConst + num_digit_repeat) /
+                        (num_training_images_ + 2 * kClassifyConst);
+    class_probabilities_[digit] = prob_digit;
+
+    for (int row = 0; row < kImageSize; row++) {
+      for (int col = 0; col < kImageSize; col++) {
+        Coordinate coord = std::make_pair(col, row);
+        int num_white = std::get<kWhiteIndex>(data_set_[digit][coord]);
+        int num_gray = std::get<kGrayIndex>(data_set_[digit][coord]);
+        int num_black = std::get<kBlackIndex>(data_set_[digit][coord]);
+        int num_repeatence = num_white + num_gray + num_black;
+
+        // Formula provided in the assignment sheet is used to calculate
+        // the probability of each coordinate in each digit for white
+        // and black/gray pixels are calculated and stored in a class
+        // variable.
+        double prob_white = (num_white + kClassifyConst) /
+                            (num_repeatence + 2 * kClassifyConst);
+        double prob_gray = (num_gray + kClassifyConst) /
+                           (num_repeatence + 2 * kClassifyConst);
+        double prob_black = (num_black + kClassifyConst) /
+                            (num_repeatence + 2 * kClassifyConst);
+        probability_set_[digit][coord] = std::make_tuple(
+            log(prob_white),
+            log(prob_gray),
+            log(prob_black));
+      }
+    }
+  }
+}
 
 
 
