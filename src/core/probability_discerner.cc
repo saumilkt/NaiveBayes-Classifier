de67b3d93e094571c8bef76ac6496994cf3346f9
Saumil Thakore <saumilkt@gmail.com>
2020-10-13 18:48:04 -0700

    Added functions to handle user input

--- src/core/probability_discerner.cc
+++ src/core/probability_discerner.cc
@@ -49,6 +49,74 @@ void probability_discerner::InitializeProbabilitySet() {
 
 }
 
+void probability_discerner::CommandLineInterface(){
+  while (true) {
+    std::cout<< "Please Enter 0 to read training data to create a model, "
+                "1 to write an existing model to a file, 2 to read an existing "
+                "model from a file, or 3 to end" << std::endl;
+    int user_choice;
+    try {
+      std::string input;
+      getline(std::cin, input);
+      user_choice = std::stoi(input);
+      if(user_choice == 3) break;
+    } catch (std::invalid_argument& e) {
+      std::cout << "Please give a valid input" << std::endl;
+      continue;
+    }
+
+    ProcessInput(user_choice);
+  }
+}
+
+void probability_discerner::ProcessInput(const int &user_choice){
+  switch (user_choice) {
+    case 0:{
+      std::cout << "Please provide a file path for data" << std::endl;
+      std::string data_path;
+      getline(std::cin, data_path);
+      if (data_path == ""){
+        //Default values are used when value entered is empty.
+        ImportData(kDefTrainDataPath,kDefTrainLabelPath);
+      } else {
+        std::cout << "Please provide a file path"
+                     " for training labels" << std::endl;
+        std::string label_path;
+        getline(std::cin, label_path);
+        ImportData(data_path, label_path);
+      }
+      return;
+
+    } case 1:{
+      std::cout << "Please provide a file path to write a model " << std::endl;
+      std::string file_path;
+      getline(std::cin, file_path);
+      if(file_path == ""){
+        //Default value is used when value entered is empty.
+        WriteModelToFile(kDefModelWriteReadPath);
+      } else {
+        WriteModelToFile(file_path);
+      }
+      return;
+
+    } case 2:{
+      std::cout << "Please provide a path to read a model from " << std::endl;
+      std::string file_path;
+      getline(std::cin, file_path);
+      if(file_path == ""){
+        //Default value is used when value entered is empty.
+        ImportModelFromFile(kDefModelWriteReadPath);
+      } else {
+        ImportModelFromFile(file_path);
+      }
+      return;
+    } default:
+      std::cout << "Please enter a valid input"<< std::endl;
+      return;
+  }
+}
+
+
 
 
 } // namespace naivebayes
