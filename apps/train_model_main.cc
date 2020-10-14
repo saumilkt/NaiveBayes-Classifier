86dc132a90595eea2e101eb042a655a1bfac45da
Saumil Thakore <saumilkt@gmail.com>
2020-10-13 18:53:03 -0700

    Changed main to create a model and write it to a file

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -1,13 +1,9 @@
-#include <iostream>
+#include <core/probability_discerner.h>
 
-#include <core/rename_this_file.h>
-
-// TODO: You may want to change main's signature to take in argc and argv
 int main() {
-  // TODO: Replace this with code that reads the training data, trains a model,
-  // and saves the trained model to a file.
+  naivebayes::probability_discerner().ImportData("data/trainingimages",
+                                                 "data/traininglabels");
+  naivebayes::probability_discerner().WriteModelToFile("data.model.txt");
 
-  std::cout << "Welcome to " << naivebayes::Placeholder().GetBestClass()
-            << std::endl;
   return 0;
 }
