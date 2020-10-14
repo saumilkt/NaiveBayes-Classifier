a3856dd2ca9a477096b7f7f8a24f63bf729f4163
Saumil Thakore <saumilkt@gmail.com>
2020-10-13 18:39:54 -0700

    Added public methods and vars to header file

--- include/core/probability_discerner.h
+++ include/core/probability_discerner.h
@@ -7,7 +7,7 @@
 #include <tuple>
 
 namespace naivebayes {
-
+using std::map;
 
 typedef std::pair<int, int> Coordinate;
 
@@ -33,6 +33,41 @@ const std::string kDefModelWriteReadPath = "../data/datamodel.txt";
 
 class probability_discerner {
  public:
+  int num_training_images_;
+
+  // Maps representing class probabilites, raw pixel data, and pixel
+  // probabilities
+  map<int, double> class_probabilities_;
+  map<int, map<Coordinate, std::tuple<int, int, int>>> data_set_;
+  map<int, map<Coordinate,
+      std::tuple<double, double, double>>> probability_set_;
+
+  // formatting for models
+  const char pixel_separator_ = ' ';
+  const char coord_separator_ = ',';
+
+  // Constructor, initializes datasets
+  probability_discerner();
+
+
+  // These methods are set to public for testing.
+
+  // Converts data from files to a model for naive bayes algorithm.
+  // Takes 2 std::strings that represent the label and data files path.
+  // Values in data_set_ are updated according to the data and labels.
+  void ImportData(const std::string &data_path,
+                  const std::string &label_path);
+
+  // Writes the existing data_set_ and number of examples to a file.
+  // Takes an std::string that represents the file path as the only argument.
+  // Returns a boolean that represents if the file writing was succesfull.
+  bool WriteModelToFile(const std::string &file_path);
+
+  // Uses the same file format as WriteModelToFile to import a data_set_.
+  // Takes an std::string that represents the file path as the only argument.
+  // Returns a boolean that represents if the file reading was succesfull.
+  bool ImportModelFromFile(const std::string &file_path);
+
 
 };
 
