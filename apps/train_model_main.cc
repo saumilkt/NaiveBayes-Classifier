46a3f12b1a8bf81cd7163d2e74dc6381ee349dc0
Saumil Thakore <saumilkt@gmail.com>
2020-10-19 22:09:34 -0700

    added command line parsing for week1 methods

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -1,9 +1,19 @@
 #include <core/probability_discerner.h>
+#include <stdexcept>
+#include <iostream>
+const size_t kNumberOfArgs =4;
 
-int main() {
-  naivebayes::probability_discerner().ImportData("data/trainingimages",
-                                                 "data/traininglabels");
-  naivebayes::probability_discerner().WriteModelToFile("data.model.txt");
-
+int main(int argc, char* argv[]) {
+  naivebayes::probability_discerner discerner;
+  std::string argument = argv[1];
+  if (argument==("train")) {
+   discerner.ImportData(argv[2] ,argv[3])
+  } else if (argument.compare("read")) {
+    discerner.ImportModelFromFile(argv[2]);
+  } else if (argument.compare("write")) {
+    discerner.WriteModelToFile(argv[2]);
+  } else {
+    throw std::invalid_argument("The command you inputted is not found");
+  }
   return 0;
 }
