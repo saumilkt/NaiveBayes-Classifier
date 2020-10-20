07e4763b510d32b5bdbeaeaadcfa0b18f81c8e31
Saumil Thakore <saumilkt@gmail.com>
2020-10-20 00:44:59 -0700

    Adjusted syntax in main

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -1,13 +1,12 @@
 #include <core/probability_discerner.h>
 #include <stdexcept>
-#include <iostream>
-const size_t kNumberOfArgs =4;
+
 
 int main(int argc, char* argv[]) {
   naivebayes::probability_discerner discerner;
   std::string argument = argv[1];
   if (argument==("train")) {
-   discerner.ImportData(argv[2] ,argv[3])
+   discerner.ImportData(argv[2] ,argv[3]);
   } else if (argument.compare("read")) {
     discerner.ImportModelFromFile(argv[2]);
   } else if (argument.compare("write")) {
