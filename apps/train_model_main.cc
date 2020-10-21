de52da3c101a807a6f3101a5bda44938743bef71
Saumil Thakore <saumilkt@gmail.com>
2020-10-20 20:07:46 -0700

    Added cmd line support for Classifying images

--- apps/train_model_main.cc
+++ apps/train_model_main.cc
@@ -5,14 +5,17 @@
 int main(int argc, char* argv[]) {
   naivebayes::Model discerner;
   std::string argument = argv[1];
+  
   if (argument==("train")) {
-   discerner.ImportData(argv[2] ,argv[3]);
-  } else if (argument.compare("read")) {
+    discerner.ImportData(argv[2] ,argv[3]);
+  } else if (argument==("read")) {
     discerner.ImportModelFromFile(argv[2]);
-  } else if (argument.compare("write")) {
+  } else if (argument==("write")) {
     discerner.WriteModelToFile(argv[2]);
+  } else if (argument==("classify")) {
+    discerner.ClassifyImages(argv[2] ,argv[3]);
   } else {
-    throw std::invalid_argument("The command you inputted is not found");
+      throw std::invalid_argument("The command you inputted is not found");
   }
   return 0;
 }
