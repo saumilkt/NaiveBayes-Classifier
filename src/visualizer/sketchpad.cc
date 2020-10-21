53234d1b6248aa797025cadf29a94c3c9f9d4589
Saumil Thakore <saumilkt@gmail.com>
2020-10-20 21:08:49 -0700

    Implemented clear and handlebrush functions

--- src/visualizer/sketchpad.cc
+++ src/visualizer/sketchpad.cc
@@ -16,8 +16,6 @@ Sketchpad::Sketchpad(const vec2& top_left_corner, size_t num_pixels_per_side,
 void Sketchpad::Draw() const {
   for (size_t row = 0; row < num_pixels_per_side_; ++row) {
     for (size_t col = 0; col < num_pixels_per_side_; ++col) {
-      // Currently, this will draw a quarter circle centered at the top-left
-      // corner with a radius of 20
 
       // TODO: Replace the if-statement below with an if-statement that checks
       // if the pixel at (row, col) is currently shaded
@@ -52,14 +50,44 @@ void Sketchpad::HandleBrush(const vec2& brush_screen_coords) {
 
       if (glm::distance(brush_sketchpad_coords, pixel_center) <=
           brush_radius_) {
-        // TODO: Add code to shade in the pixel at (row, col)
+        ci::gl::color(ci::Color::gray(0.3f));
+        vec2 pixel_top_left = top_left_corner_ +
+                              vec2(col * pixel_side_length_,
+                                   row * pixel_side_length_);
+
+        vec2 pixel_bottom_right =
+            pixel_top_left + vec2(pixel_side_length_, pixel_side_length_);
+        ci::Rectf pixel_bounding_box(pixel_top_left, pixel_bottom_right);
+
+        ci::gl::drawSolidRect(pixel_bounding_box);
+
+        ci::gl::color(ci::Color("black"));
+        ci::gl::drawStrokedRect(pixel_bounding_box);
       }
     }
   }
 }
 
 void Sketchpad::Clear() {
-  // TODO: implement this method
+  ci::gl::clear(ci::Color("white"));
+
+  for (size_t row = 0; row < num_pixels_per_side_; ++row) {
+    for (size_t col = 0; col < num_pixels_per_side_; ++col) {
+      ci::gl::color(ci::Color("white"));
+
+      vec2 pixel_top_left = top_left_corner_ + vec2(col * pixel_side_length_,
+                                                    row * pixel_side_length_);
+
+      vec2 pixel_bottom_right =
+          pixel_top_left + vec2(pixel_side_length_, pixel_side_length_);
+      ci::Rectf pixel_bounding_box(pixel_top_left, pixel_bottom_right);
+
+      ci::gl::drawSolidRect(pixel_bounding_box);
+
+      ci::gl::color(ci::Color("black"));
+      ci::gl::drawStrokedRect(pixel_bounding_box);
+    }
+  }
 }
 
 }  // namespace visualizer
