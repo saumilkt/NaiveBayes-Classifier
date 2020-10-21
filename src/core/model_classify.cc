3efd4e8425ad6e8f988583bf2ec534084466ad22
Saumil Thakore <saumilkt@gmail.com>
2020-10-20 20:05:31 -0700

    Implemented ClassifyImages()

--- src/core/model_classify.cc
+++ src/core/model_classify.cc
@@ -3,8 +3,11 @@
 //
 
 #include "core/model_classify.h"
-#include "core/model_analyze.h"
 
+#include <fstream>
+#include <iostream>
+
+#include "core/model_analyze.h"
 namespace naivebayes {
 double Model::GetDigitProbability(const int &digit,
                                   std::map<Coordinate,int> &image_set) {
@@ -31,7 +34,7 @@ double Model::GetPixelProbability(const int &digit, const int &color,
   }
 
   // Otherwise the color of the pixel is black or gray, so the corresponding
-  // value is the second +thrid in the tuple.
+  // value is the second +third in the tuple.
   return std::get<kGrayIndex>(probability_set_[digit][coord]) +
       std::get<kBlackIndex>(probability_set_[digit][coord]);
 }
@@ -52,5 +55,45 @@ int Model::GetMostLikelyDigit(std::map<Coordinate ,int> &image_set){
 
   // Digit with the highest probability is returned.
   return digit_highest_prob;
+}
+
+void Model::ClassifyImages(const std::string &file_path,
+                                                const std::string &label_path) {
+  std::ifstream train_file(file_path);
+  std::string line;
+  std::ifstream label_file(label_path);
+  std::string label;
+
+  // These two values are used to calculate the number of correct guesses.
+  int correct_digits = 0;
+  int num_test_img = 0;
+
+  while(!label_file.eof()) {
+    num_test_img++;
+    int label_ind = stoi(label);
+    std::map<Coordinate ,int> img;
+    for (int row = 0; row < kImageSize; row++) {
+      std::getline(train_file,line);
+      for (int col = 0; col < kImageSize; col++) {
+        Coordinate coord = std::make_pair(col, row);
+        if (line.at(col) == kGrayPixel ||
+           line.at(col) == kBlackPixel){
+          img[coord] = '#';
+        } else{
+          img[coord] = ' ';
+        }
+      }
+    }
+    // Most likely digit is found is used to add values to confusion matrix
+    // and to calculate correctness percentage.
+    int most_likely_digit = GetMostLikelyDigit(img);
+    if (most_likely_digit == label_ind) {
+      correct_digits++;
+    }
+  }
+
+  // A percentage of how correct the algorithm is printed to inform the user.
+  double percentage = 100 * correct_digits/num_test_img;
+  std::cout << percentage <<"% accurate" <<std::endl;
 }
 } // namespace naivebayes
